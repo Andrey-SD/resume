7f3d62ea0411769b4bafa87018e0eabbe1a83cd0
Andrey <andrey.sd.post@gmail.com>
2018-02-22 00:03:49 +0200

    fixed edit data.xml

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,10 +2,10 @@
 @section('content')
 	<form class="form-horizontal" method="POST" action="{{ route('code_edit') }}">
     {{ csrf_field() }}
-		<div class="form-group">
+		
 			<textarea class="form-control" rows="25" name="code">{{$data}}</textarea>
-		</div>
-		<button style="width:100%" type="submit" class="btn btn-primary">
+		
+		<button style="width:100%" type="submit">
 			Save
 		</button>
 	</form>
