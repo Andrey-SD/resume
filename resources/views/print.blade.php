36f9a25931961d9a8535054ccfebf4b1dad25ab8
Andrey <andrey.sd.post@gmail.com>
2018-04-19 00:36:49 +0300

    final print page

--- resources/views/print.blade.php
+++ resources/views/print.blade.php
@@ -7,7 +7,7 @@
 	<div>
 		<table>
 			<tr>
-				<td>Телефон</td>
+				<td>Телефон (Viber)</td>
 				<td>{{$info->phone_number}}</td>
 			</tr>
 			<tr>
@@ -37,6 +37,52 @@
 		</table>
 		<img src='{{ URL::asset('img/photo.png')}}'>
 	</div>
+		<div class="rw">
+			<p>Образование:</p>
+			<div>
+				@foreach($info->education as $education)
+					<p>Период: {{$education->date}}</p>
+					<p>Место учебы: {{$education->name}}</p>
+					<p>Специальность: {{$education->lesson}}</p>
+				@endforeach
+			</div>
+		</div>
+		<div class="rw">
+			<p>Навыки:</p>
+			<div class="skills">
+				<dl>
+					<dt>HTML</dt>
+					<dt>CSS</dt>
+						<ul>
+							<li>Bootstrap</li>
+						</ul>
+					<dt>JavaScript</dt>
+						<ul>
+							<li>Ajax</li>
+							<li>JQuery</li>
+							<li>DOM</li>
+						</ul>
+					<dt>MYSQL</dt>
+					<dt>PHP</dt>
+						<ul>
+							<li>Laravel</li>
+						</ul>
+					<dt>GitHub</dt>
+				</dl>
+			</div>
+		</div>
+		<div class="rw">
+			<p>Опыт работы:</p>
+			<div>
+				@foreach($info->works as $work)
+					<p class="frs">{{$work->date}}</p>
+					<p class="scn">{{$work->name}}</p>
+					<p class="tr">{{$work->position}}</p>
+					<p style="border-bottom: 1px dotted #8B8B8B; margin-bottom:5mm !important;">{{$work->duties}}</p>
+				@endforeach
+			</div>			
+		</div>
+	
 </section>
 <button>Печать</button>
 
