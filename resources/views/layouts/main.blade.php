7e7cf2df0ea422b6c44eed5eb7e1acc40bf8a8e0
Andrey-SD <andrey.sd.post@gmail.com>
2022-04-01 21:43:25 +0300

    update resume

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,8 +14,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-	<link href='/css/main.css' rel="stylesheet" type="text/css">
-	<!-- test -->
+	<link href='{{ URL::asset('css/main.css') }}' rel="stylesheet" type="text/css">
 	<link href='{{ URL::asset('css/print.css') }}' rel="stylesheet" type="text/css">
 </head>
 
